c12549918158c87b499e2014d30262afebdbd359
vihanga nivarthana <vihanganivarthana@vihangas-MacBook-Air.local>
2020-03-21 14:29:12 +0530

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Hero></Hero>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Hero from './components/Hero'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   Hero,
   }
 }
 </script>
 
 <style>
+
+@font-face {
+  font-family: 'Product Sans';
+  font-style: normal;
+  font-weight: 400;
+  src: local('Open Sans'), local('OpenSans'), url(https://fonts.gstatic.com/s/productsans/v5/HYvgU2fE2nRJvZ5JFAumwegdm0LZdjqr5-oayXSOefg.woff2) format('woff2');
+}
+
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Product Sans;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+margin:0;
+padding:0;
 }
 </style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,33 @@
+<template>
+    <section id="hero">
+        <div class="main_title">
+             Corona OutBreak SriLanka
+        </div>
+    </section>
+</template>
+
+<script>
+
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+*{
+    margin:0;
+    padding: 0;
+}
+#hero{
+    height:100vh;
+    background: #409bf0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.main_title{
+font-size: 40px;
+color:#ffffff;
+}
+</style>
